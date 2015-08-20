215bd79c6a14bda7d77b4500ee3e9750ae802d32
Kalevyx <crli@NN123-HA-01.HS.sd203.org>
2015-08-20 15:07:52 -0500

    Explored Turtle
    Made triangle

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,13 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.BLUE);
         turtle.penDown();
-        turtle.forward(50);
+        turtle.turn(30);
+        turtle.forward(200);
+        turtle.turn(120);
+        turtle.forward(200);
+        turtle.turn(120);
+        turtle.forward(195);
     }
 }
