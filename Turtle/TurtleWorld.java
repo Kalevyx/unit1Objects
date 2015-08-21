2a1e613d8efbd3468301f59f7d63d01b6be58de0
Kalevyx <crli@NN123-HA-01.HS.sd203.org>
2015-08-21 15:07:46 -0500

    Explored - 8-21-15
    More triangles

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -7,12 +7,29 @@ public class TurtleWorld
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
         turtle.setPenColor(Color.BLUE);
+        turtle.setBodyColor(Color.pink);
         turtle.penDown();
         turtle.turn(30);
         turtle.forward(200);
         turtle.turn(120);
         turtle.forward(200);
         turtle.turn(120);
-        turtle.forward(195);
+        turtle.forward(200);
+        turtle.penUp();
+        turtle.turnRight();
+        turtle.forward(115);
+        turtle.penDown();
+        turtle.turnRight();
+        turtle.forward(200);
+        turtle.turn(120);
+        turtle.forward(200);
+        turtle.turn(120);
+        turtle.forward(200);
+        turtle.turnRight();
+        turtle.penUp();
+        turtle.forward(60);
+        turtle.turn(60);
+        turtle.penDown();
+        
     }
 }
