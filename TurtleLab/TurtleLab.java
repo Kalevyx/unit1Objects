cfe082541b63ed6368211ff0da900e5e54d61396
Kalevyx <crli@NN123-HA-01.HS.sd203.org>
2015-09-02 15:08:41 -0500

    Turtle Lab

---
+++ TurtleLab/TurtleLab.java
@@ -0,0 +1,11 @@
+// http://sites.asmsa.org/java-turtle/documentation
+
+public class TurtleLab
+{
+    public static void main(String[] args)
+    {
+        World turtleWorld = new World();
+        Turtle turtle1 = new Turtle(turtleWorld);
+        Turtle turtle2 = new Turtle(turtleWorld);
+    }
+}
