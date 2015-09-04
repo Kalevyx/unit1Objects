93b88758c4e41e668b53d6c5973ec4e4d9329986
Kalevyx <crli@NN123-HA-01.HS.sd203.org>
2015-09-04 15:03:15 -0500

    Turtle Lab

--- TurtleLab/TurtleLab.java
+++ TurtleLab/TurtleLab.java
@@ -7,7 +7,7 @@ public class TurtleLab
         World turtleWorld = new World();
         Turtle turtle1 = new Turtle(turtleWorld);
         Turtle turtle2 = new Turtle(turtleWorld);
-        //turtle1.hide();
+        turtle1.hide();
         turtle1.forward(100);
         turtle1.turn(-45);
         turtle1.forward(100);
@@ -27,8 +27,7 @@ public class TurtleLab
         turtle1.penDown();
         turtle1.forward(100);
         turtle1.backward(200);
-        
-        //turtle2.hide();
+        turtle2.hide();
         turtle2.backward(100);
         turtle2.turn(135);
         turtle2.forward(100);
